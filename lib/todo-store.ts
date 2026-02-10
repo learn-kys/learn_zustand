738ae982ee3dab0339a6911286463c99897d60f0
KYs <amitkys59@gmail.com>
2026-02-10 23:00:58 +0530

    todo: user can add todo, stores ready

--- lib/todo-store.ts
+++ lib/todo-store.ts
@@ -10,17 +10,18 @@ const todoDB: Todo[] = [
   { id: "123", text: "firstTodo", isCompleted: false },
   { id: "124", text: "hi there", isCompleted: true },
 ];
+todoDB.push({ id: "125", text: "this is 3rd todo", isCompleted: true });
 
 interface ITodo {
   todos: Todo[];
   toggleTodo: (id: string) => void;
   deleteTodo: (id: string) => void;
   updateTodo: (id: string, text: string) => void;
-  // addTodo: (todo: Todo) => void;
+  addTodo: (todo: Todo) => void;
 }
 
 export const useTodo = create<ITodo>((set) => ({
-  todos: todoDB,
+  todos: [...todoDB],
   toggleTodo: (id) =>
     set((state) => ({
       todos: state.todos.map((todo) =>
@@ -38,6 +39,12 @@ export const useTodo = create<ITodo>((set) => ({
       todos: state.todos.map((todo) =>
         todo.id === id ? { ...todo, text: text } : todo,
       ),
+    }));
+  },
+
+  addTodo: (todo) => {
+    set((state) => ({
+      todos: [...state.todos, todo],
     }));
   },
 }));
