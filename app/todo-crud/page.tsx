c6da4c57f3d0a1f372a8eb1395d506094752bcee
KYs <amitkys59@gmail.com>
2026-02-10 21:16:02 +0530

    listed and handle toggle on todo

---
+++ app/todo-crud/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { useTodo } from "@/lib/todo-store";
+
+export default function Page() {
+  const todos = useTodo((state) => state.todos);
+  const toggleTodo = useTodo((state) => state.toggleTodo);
+  return (
+    <div>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <p>
+            <b>
+              {" "}
+              {todo.isCompleted ? <del>{todo.text}:</del> : `${todo.text}:`}
+            </b>{" "}
+            {todo.isCompleted ? "completed" : "not completed"}{" "}
+          </p>
+          <button type="button" onClick={() => toggleTodo(todo.id)}>
+            {todo.isCompleted ? "mark as completed" : "mark as Incompleted"}
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ lib/todo-store.ts
@@ -0,0 +1,27 @@
+import { create } from "zustand";
+
+interface Todo {
+  id: string;
+  text: string;
+  isCompleted: boolean;
+}
+
+const todoDB: Todo[] = [{ id: "123", text: "firstTodo", isCompleted: false }];
+
+interface ITodo {
+  todos: Todo[];
+  toggleTodo: (id: string) => void;
+  // deleteTodo: (id: string) => void;
+  // updateTodo: (id: string) => void;
+  // addTodo: (todo: Todo) => void;
+}
+
+export const useTodo = create<ITodo>((set) => ({
+  todos: todoDB,
+  toggleTodo: (id) =>
+    set((state) => ({
+      todos: state.todos.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo,
+      ),
+    })),
+}));
