4f49879becbe81f609df6d5507f5fd481b39a31a
KYs <amitkys59@gmail.com>
2026-02-10 23:36:45 +0530

    todo: crud done

--- app/todo-crud/page.tsx
+++ app/todo-crud/page.tsx
@@ -1,17 +1,33 @@
 "use client";
 import { useTodo } from "@/lib/todo-store";
+import { generatedRandomString } from "@/lib/utils";
 
 export default function Page() {
   const todos = useTodo((state) => state.todos);
   const toggleTodo = useTodo((state) => state.toggleTodo);
   const deleteTodo = useTodo((state) => state.deleteTodo);
   const updateTodo = useTodo((state) => state.updateTodo);
-  const handleUpdate = (id: string) => {
-    const userInput = prompt("Enter todo name");
-    if (userInput !== null) {
+  const addTodo = useTodo((state) => state.addTodo);
+
+  const handleUpdate = (id: string, text: string) => {
+    const userInput = prompt("Enter todo name", text);
+    if (userInput) {
       updateTodo(id, userInput);
     } else {
       alert("user do not want to update");
+    }
+  };
+
+  const handleAddTodo = () => {
+    const userInputText = prompt("Enter todo name");
+    if (userInputText) {
+      addTodo({
+        id: generatedRandomString(8),
+        text: userInputText,
+        isCompleted: false,
+      });
+    } else {
+      alert("user do not want to add todo");
     }
   };
   return (
@@ -30,12 +46,19 @@ export default function Page() {
             {todo.isCompleted ? "mark as Incompleted" : "mark as completed"}
           </button>
           {/* update */}
-          <button type="button" onClick={() => handleUpdate(todo.id)}>
+          <button
+            type="button"
+            onClick={() => handleUpdate(todo.id, todo.text)}
+          >
             update
           </button>
           {/* delete */}
           <button type="button" onClick={() => deleteTodo(todo.id)}>
             Delete
+          </button>
+          {/* add new todo  */}
+          <button type="button" onClick={handleAddTodo}>
+            Add new todo
           </button>
           <hr />
         </div>
